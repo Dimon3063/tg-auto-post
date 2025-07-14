a1ecd6f8e67b428408746db54551b51f991b3a00
Dimon3063 <dimonkilimnik@gmail.com>
2025-07-14 14:15:13 +0300

    Update post.py

--- post.py
+++ post.py
@@ -4,12 +4,15 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID   = os.getenv("CHAT_ID")
 TIME      = datetime.datetime.utcnow() + datetime.timedelta(hours=3)
 
-# ↓↓↓ ось саме посилання на gist
-url = f"https://gist.githubusercontent.com/Dimon3063/f53bcb68373890e891cd141d1475008b/raw/post.txt?t={int(datetime.datetime.utcnow().timestamp())}"
-
+# Читаємо свіжий пост із Google-таблиці
+url = "https://docs.google.com/spreadsheets/d/1eXPiP55es1oCycvh6RPNSboYXLw-vlFq27ZFfP8re0Q/gviz/tq?tqx=out:csv&gid=0&range=A2"
 text = requests.get(url, timeout=10).text.strip()
+
 if not text or text.lower() == "skip":
     exit(0)
+
+# замінюємо {{DATE}} на поточну дату
+text = text.replace("{{DATE}}", TIME.strftime("%d.%m %H:%M"))
 
 requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
